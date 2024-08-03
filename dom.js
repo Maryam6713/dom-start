5f9f42d955ab8a26155ef7cbe2c6eddc499c4cda
Maryam6713 <maryammalik6713@gmail.com>
2024-08-03 15:14:47 +0500

    first commit

---
+++ dom.js
@@ -0,0 +1,82 @@
+//console.log(document.childNodes[1].childNodes[2].childNodes);
+
+//console.log(document.documentElement);
+//console.log(document.body);
+//console.log(document.head);
+
+//console.log(document.body.childNodes[1]);
+
+//console.log(document.body.childNodes[1].childNodes[0].parentNode);
+//console.log(document.head.parentNode);
+
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastElementChild);
+
+// var arr = Array.from(document.body.children)
+// console.log(arr);
+
+// console.log(document.body.childNodes);
+// console.log(document.body.children);
+
+// console.log(document.body.childNodes[0]);
+// console.log(document.body.children[0].childNodes.childNodes);//undedined
+// console.log(document.body.children[0].children.children);//undedined
+
+
+// function change(event){
+//     var box = document.getElementById('box')
+//     box.remove()
+// }
+// function change(event){
+//     var image = document.querySelector("#box")
+//     image.remove() 
+  
+//     var blockText = document.querySelector('.heading')
+//     blockText.style.display = "block"
+//     blockText.innerHTML = "Blocked"
+        
+//      }
+// text.style.display = "block"
+
+// function change(event){
+//     var box = document.querySelector("#box")
+//     box.remove()
+
+//     var text = document.querySelector(".heading")
+//     text.style.display = "block"
+//     text.innerHTML = "ayesha"
+// }
+
+// function change(event){
+//     var box = document.querySelector('body')
+//     box.children[0].remove()
+
+//     box.children[1].style.display = "block"
+//     box.children[1].innerHTML = "ayesha"
+// }
+
+// function change(event){
+//     var box = document.querySelector('body')
+//     box.children[0].childNodes[0].remove()
+
+//     box.children[0].childNodes[1].style.display = "block"
+//     box.children[0].childNodes[1].innerHTML = "Ayesha"
+// 
+
+
+
+// function changes(event){
+//     var text = document.querySelector("#text")
+//   text.parentNode.childNodes[1].remove() 
+    
+// }
+
+// console.log(text.parentNode);
+
+console.log(document.childNodes[1].childNodes[1].childNodes[3])
+var heading = document.getElementById('heading')
+console.log(heading.nodeType);
+console.log(heading.nodeName.toLocaleLowerCase());
